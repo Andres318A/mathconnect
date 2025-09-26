1174ad135892396c89bacc906a63a7042cba6376
Andres318A <a.gonzalezcam@udla.edu.co>
2025-09-26 18:29:31 -0500

    ase

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
     <div className="home-background">
       <div className="centered-content">
         <h1 className="home-title">
-          ¡Bienvenido asf<br/>a<br/>MathConnect!
+          ¡Bienvenido<br/>a<br/>MathConnect!
         </h1>
         <div className="home-info">
           <p><strong>MathConnect</strong> es una plataforma web educativa diseñada para facilitar el aprendizaje interactivo de los conceptos de límites y continuidad en funciones reales. El aplicativo integra recursos teóricos claros, simulaciones visuales y ejemplos prácticos, permitiendo a estudiantes y docentes explorar de manera intuitiva y dinámica los principales temas del cálculo. MathConnect promueve la comprensión empírica y la experimentación matemática en un entorno accesible, moderno y adaptado a las necesidades de la formación.</p>
